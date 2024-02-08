3c9ee1637e958355cc45fd81260f991b404e10c0
filzrev <103790468+filzrev@users.noreply.github.com>
2024-02-08 12:16:51 +0900

    Update SampleEnums.cs

--- src/SampleLib/SampleEnums.cs
+++ src/SampleLib/SampleEnums.cs
@@ -16,7 +16,19 @@ public enum SampleEnum1
 public enum SampleEnum2
 {
     None = 0,
+    
+    /// <summary>
+    /// Description1for EnumB.
+    </summary>
     EnumB = 0x01
+    
+    /// <summary>
+    /// Description1for EnumB.
+    /// </summary>
     EnumD = 0x02,
+    
+    /// <summary>
+    /// Description1for EnumA.
+    /// </summary>
     EnumA = 0x04,
 }
